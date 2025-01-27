0e909f44b79220f49d0590ce2b2f728d3ff76df2
Fouadddiv <boukafouad4@gmail.com>
2025-01-27 14:49:46 +0100

    Add files via upload

---
+++ logIn.js
@@ -0,0 +1,8 @@
+function changing(){
+    return document.getElementById("tp").innerHTML="helo"
+ }
+ function Return(){
+    return document.getElementById("tp").innerHTML="WELCOME AGAIN"
+  }
+  function verify(){
+   let vu=document.getElementById("un").value;return vu   }
